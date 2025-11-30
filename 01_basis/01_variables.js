6b69d5abf75adbe16e81bd58fa50b1ca34466842
Tapojay <tapojaysardar@gmail.com>
2025-11-30 19:52:41 +0000

    container config done

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 113324
+let accountEmail = "tapojaysardar@gmail.com"
+var accountPassoword = "1234"
+accountCity = "Kolkata"
+
+//accountId = 2
+accountEmail = "hc@hc.com"
+accuntPassoword = "212122"
+accountCity = "jaipur"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassoword, accountCity])
